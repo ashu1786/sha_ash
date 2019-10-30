79c3c45c01df633b2eb4b48b551bfe96908228ac
ashu1786 <57189719+ashu1786@users.noreply.github.com>
2019-10-30 22:38:14 +0530

    Update main.c

--- main.c
+++ main.c
@@ -9,12 +9,12 @@ Write your code in this editor and press "Run" button to compile and execute it.
 #include<stdio.h>
 
 int main() {int i,j
-scanf("%d",&i);
+scanf("%c",&i);
        if(i%2==0)
        {
         printf("no is even:%d",i);
        }else{
-       printf("no is odd:%d",i);
+       printf("no is odd:%d",i;
        }
     
   }
