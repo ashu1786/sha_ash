2d0ac6915438e6389ec251d8257058c9120d50e9
ashu1786 <57189719+ashu1786@users.noreply.github.com>
2019-10-30 22:24:22 +0530

    Update main.c

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 #include<stdio.h>
 
-int main() {int i;
+int main() {int i,j;
 scanf("%d",&i);
        if(i%2==0)
        {
