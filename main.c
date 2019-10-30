d1d7f0206b8b3e4d782e76b5e2670a663d3ab609
ashu1786 <57189719+ashu1786@users.noreply.github.com>
2019-10-30 22:08:15 +0530

    read.mhh

---
+++ main.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include<stdio.h>
+
+int main() {int i;
+scanf("%d",&i);
+       if(i%2==0)
+       {
+        printf("no is even");
+       }else{
+       printf("no is odd");
+       }
+    
+  }
